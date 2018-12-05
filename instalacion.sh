765737488e519217fc47f67765f460b5b0458b31
Sergio Herrero <sergio.herrero.barco@gmail.com>
2018-12-05 10:16:54 +0000

    Nueva versión de instalacion.sh

---
+++ instalacion.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+printf "\n\nActualizando paquetes\n\n"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+printf '\n\nINSTALACIÓN DE JAVA, SQL y GIT\n\n'
+sudo apt-get install default-jdk -y
+
+sudo apt-get install software-properties-common
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://ftp.utexas.edu/mariadb/repo/10.3/ubuntu bionic main'
+
+sudo apt update
+
+sudo apt install mariadb-server 
+sudo apt-get install git
+
+printf '\n\nINSTALACIÓN DE ECOUNIZAR\n\n'
+mkdir ecoUnizar
+cd ecoUnizar
+
+git clone 'https://github.com/sherrero96/EcoUnizar.git'
+cd EcoUnizar
+
+
+echo '\n\n--------OPERACIONES DE ROOT--------\n\n'
+sudo mysql -u root -p -e "CREATE DATABASE sistemasinformacion"
+sudo mysql -u root -p -e "CREATE USER 'usuario'@'localhost' IDENTIFIED BY 'password'"
+sudo mysql -u root -p -e "Grant ALL ON sistemasinformacion.* TO usuario@localhost"
+
+cd DB
+echo '\n\n--------OPERACIONES DE usuario--------\n\n'
+echo '\n\n--------clave: password--------\n\n'
+mysql -u usuario -p sistemasinformacion < Database/creacionTablas.sql
+mysql -u usuario -p sistemasinformacion < Database/crearUsuariosIniciales.sql --force
+
+mysql -u usuario -p -e 'USE sistemasinformacion'
+
+# Instalacion de TOMCAT
+wget http://apache.rediris.es/tomcat/tomcat-8/v8.5.35/bin/apache-tomcat-8.5.35.tar.gz
+sudo tar -zxvf apache-tomcat-8.5.35.tar.gz -C /var/local/
+sudo /var/local/apache-tomcat-8.5.35/bin/startup.sh
