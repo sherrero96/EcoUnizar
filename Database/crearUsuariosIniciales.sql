8952f768c2923c2dc4e0cdbb35e357c7f98fa7e8
Sergio Herrero <sergio.herrero.barco@gmail.com>
2018-12-10 20:00:04 +0100

    Update crearUsuariosIniciales.sql

--- Database/crearUsuariosIniciales.sql
+++ Database/crearUsuariosIniciales.sql
@@ -1,2 +1,2 @@
-INSERT INTO Grupo (nombre) VALUES ('NoCarteles');
-INSERT INTO Usuario (correo,password,rol,nombre,nacimiento,puntuacion,idGrupo) VALUES ('admin@admin.es','admin',0,'administrador',1970,0,1)
+INSERT INTO grupo (nombre) VALUES ('NoCarteles');
+INSERT INTO usuario (correo,password,rol,nombre,nacimiento,puntuacion,idGrupo) VALUES ('admin@admin.es','admin',0,'administrador',1970,0,1)
