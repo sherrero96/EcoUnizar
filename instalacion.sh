2a1a7070934c60e0475e38346cbfaa1fd4c6a2de
Sergio Herrero <sergio.herrero.barco@gmail.com>
2018-12-11 21:36:22 +0100

    Update instalacion.sh

--- instalacion.sh
+++ instalacion.sh
@@ -10,9 +10,9 @@ sudo apt-get install software-properties-common
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://ftp.utexas.edu/mariadb/repo/10.3/ubuntu bionic main'
 
-sudo apt update
+sudo apt update -y
 
-sudo apt install mariadb-server 
+sudo apt install mariadb-server -y
 
 printf '\n\nINSTALACIÓN DE ECOUNIZAR\n\n'
 mkdir ecoUnizar
